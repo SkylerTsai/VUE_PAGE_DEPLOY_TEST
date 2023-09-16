e2b613deff4bb7405610875a9eb0d3c064b933dc
SkylerTsai <skyler17889@gmail.com>
2023-09-16 17:58:13 +0800

    First commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  outputDir: 'dist',
+  publicPath: process.env.NODE_ENV === 'production'
+	? '/VUE_PAGE_DEPLOY_TEST/'
+	: '/',
   transpileDependencies: true
 })
