b81e37907eea1c5b654088ab2af43075f92d7dd0
SkylerTsai <45330963+SkylerTsai@users.noreply.github.com>
2023-09-16 19:16:06 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  outputDir: './dist',
+  outputDir: 'dist',
   publicPath: process.env.NODE_ENV === 'production'
 	? '/VUE_PAGE_DEPLOY_TEST/'
 	: '/',
